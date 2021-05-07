ec3400ebae512da865975a9538f0bc7cfd66fb85
Maxwell Carlson <bart4282819@gmail.com>
2021-05-06 23:20:57 -0700

    Added deleting plots, untested.

--- ChiaReplotter/ChiaReplotter.py
+++ ChiaReplotter/ChiaReplotter.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import subprocess
 
@@ -7,12 +8,28 @@ import subprocess
 
 
 def replot(chiaLocation):
-    subprocess.call(['powershell', 'cd {}; ./chia.exe plots create -k {} -b {} -u {} -r {} -t {} -d {} -n {}'.format(
-        args.ChiaLocation, args.k, args.b, args.u, args.r, args.t, args.d, args.n)], shell=True)
+    cproc = subprocess.call(['powershell', 'cd {}; ./chia.exe plots create -k {} -b {} -u {} -r {} -t {} -d {} -n {}'.format(
+        args.chia_loc, args.k, args.b, args.u, args.r, args.t, args.d, args.n)], shell=True)
 
+
+# Remove first 'remove_count' *.plot files from 'remove_dir' directory
+def deletePlots(args):
+    plots = os.listdir(args.remove_dir)
+
+    i = 0
+    for p in plots:
+        if p.endswith('.plot'):
+            os.remove(p)
+            i += 1
+        if i >= args.remove_count:
+            break
 
 def run(args):
-    replot(args)
+    run = 0
+    for i in range(args.runs):
+        if args.remove_count:
+            deletePlots(args)
+        replot(args)
 
 
 if __name__ == "__main__":
@@ -24,8 +41,11 @@ if __name__ == "__main__":
     parser.add_argument('-t', type=str, help='Temp location')
     parser.add_argument('-d', type=str, help='Final location')
     parser.add_argument('-n', type=int, default=1)
-    parser.add_argument('--ChiaLocation', 
+    parser.add_argument('--chia_loc', 
                         default='C:/Users/$env:UserName/AppData/Local/chia-blockchain/app-*/resources/app.asar.unpacked/daemon')
+    parser.add_argument('--remove_count', default=0, help='Plots to remove per iteration')
+    parser.add_argument('--remove_dir', type=str)
+    parser.add_argument('--runs', default=1, 'Total iterations to run a set of delete and replots')
     args = parser.parse_args()
 
     run(args)
