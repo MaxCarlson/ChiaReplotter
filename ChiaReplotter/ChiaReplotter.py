ad64899b2a53bcbd0adc9b3898a49338e7ca81f4
Maxwell Carlson <bart4282819@gmail.com>
2021-05-06 23:07:34 -0700

    Added some command line arguments

--- ChiaReplotter/ChiaReplotter.py
+++ ChiaReplotter/ChiaReplotter.py
@@ -1,6 +1,31 @@
+import argparse
 import subprocess
 
 ".\chia.exe plots create -k 32 -b 3700 -u 128 -r 4 -t B:\Chia -d E:\Chia -n 10"
 
-subprocess.call(['powershell', 'cd C:/Users/$env:UserName/AppData/Local/chia-blockchain/app-*/resources/app.asar.unpacked/daemon;'
-    './chia.exe plots create -k 32 -b 3700 -u 128 -r 4 -t B:/Chia -d E:/Chia -n 10'], shell=True)
+'./chia.exe plots create -k 32 -b 3700 -u 128 -r 4 -t B:/Chia -d E:/Chia -n 10'
+
+
+def replot(chiaLocation):
+    subprocess.call(['powershell', 'cd {}; ./chia.exe plots create -k {} -b {} -u {} -r {} -t {} -d {} -n {}'.format(
+        args.ChiaLocation, args.k, args.b, args.u, args.r, args.t, args.d, args.n)], shell=True)
+
+
+def run(args):
+    replot(args)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-k', type=int, default=32)
+    parser.add_argument('-b', type=int, default=3400)
+    parser.add_argument('-u', type=int, default=128)
+    parser.add_argument('-r', type=int, default=2)
+    parser.add_argument('-t', type=str, help='Temp location')
+    parser.add_argument('-d', type=str, help='Final location')
+    parser.add_argument('-n', type=int, default=1)
+    parser.add_argument('--ChiaLocation', 
+                        default='C:/Users/$env:UserName/AppData/Local/chia-blockchain/app-*/resources/app.asar.unpacked/daemon')
+    args = parser.parse_args()
+
+    run(args)
